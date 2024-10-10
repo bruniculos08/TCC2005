c7542d1dd7f8281195f025c89708e1866dc58856
bruno notebook <bruniculos2014@gmail.com>
2024-10-09 21:43:04 -0300

    update

---
+++ .testingAbnTeX/abntex2/alone/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+function compileLaTeX(){
+       pdflatex -interaction=nonstopmode $1
+       bibtex $1
+       makeindex $1.idx
+       makeindex $1.nlo -s nomencl.ist -o $1.nls
+   	 makeglossaries $1
+       pdflatex -interaction=nonstopmode $1
+       pdflatex -interaction=nonstopmode $1
+		# latexmk -pdf -time -silent $1
+}
+
+compileLaTeX $1
